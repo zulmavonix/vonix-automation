f78bfc8257eb6dbdf02abbdef5ff754670219ef8
zulmavonix <128354734+zulmavonix@users.noreply.github.com>
2024-05-31 10:48:57 +0700

    Update LoginPhoneNumber.java

--- src/main/java/org/vonix/authentication/LoginPhoneNumber.java
+++ src/main/java/org/vonix/authentication/LoginPhoneNumber.java
@@ -1,4 +1,33 @@
 package org.vonix.authentication;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.vonix.Config;
 
-public class LoginPhoneNumber {
-}
+public class LoginPhoneNumber extends Config {
+
+    private static WebElement buttonLoginPhone() {
+        return driver.findElement(By.xpath("//button[normalize-space()='Login with Phone Number']"));
+    }
+
+    private static WebElement fieldPhoneNumber() {
+        return driver.findElement(By.xpath("//input[@class='vti__input']"));
+    }
+
+    private static WebElement buttonSendOTP() {
+        return driver.findElement(By.xpath("(//button[@type='submit'])[1]"));
+    }
+
+    public static WebElement textEnterOTP() {
+        return driver.findElement(By.xpath("//h1[normalize-space()='Please enter the OTP']"));
+    }
+
+    private static String inputPhoneNumber() { return "081373914540"; }
+
+    public static void stepLoginPhoneNumber() {
+        loginEmail buttonLogin  = new loginEmail();
+
+        buttonLogin.publicButtonLogin().click();
+        buttonLoginPhone().click();
+        fieldPhoneNumber().sendKeys(inputPhoneNumber());
+        buttonSendOTP().click();
+    }
